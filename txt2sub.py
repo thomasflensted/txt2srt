cfbb4cf61c31eb0db5d6a1f8db869e0d0c8e27dc
Thomas Flensted <thomasflenstedjensen@gmail.com>
2023-01-20 08:15:43 +0100

    påbegyndt separering v ny linje på ny branch

--- txt2sub.py
+++ txt2sub.py
@@ -29,6 +29,7 @@ def generate_new_text_file(in_file, new_filename):
 
     lines = get_lines_list(in_file)
 
+    '''
     with open(new_filename, "w") as new_file:
 
         seconds = 0
@@ -39,6 +40,7 @@ def generate_new_text_file(in_file, new_filename):
             timeEnd = create_timing_string(seconds)
 
             new_file.write(f"{count+1}\n{timeStart} --> {timeEnd}\n{line}\n\n")
+    '''
 
 
 def create_timing_string(seconds):
@@ -51,7 +53,12 @@ def get_lines_list(in_file):
 
     with open(in_file, "r") as in_file:
 
-        return [line.strip() for line in in_file.readlines() if len(line.strip()) > 0]
+        raw_lines = in_file.readlines()
+
+        for line in raw_lines:
+            cur_str = line
+
+        # return [line.strip() for line in in_file.readlines() if len(line.strip()) > 0]
 
 
 main()
